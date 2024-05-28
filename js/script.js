27a9ebbc534b8e7086fe636a7b6ad69dcc7fcf5b
a.rivera <a.riveraa99@gmail.com>
2024-05-28 17:11:33 -0500

    Created function that selects a random image based on a random index number

--- js/script.js
+++ js/script.js
@@ -3,10 +3,23 @@ const authorSpan = document.querySelector(".author");
 const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
 
+// getting the image
 const getImage = async function() {
     const res = await fetch("https://picsum.photos/v2/list?limit=100");
     const images = await res.json();
     console.log(images);
+
+    // calling the number to get a random number
+    selectRandomImage(images);
 };
 
 getImage();
+
+const selectRandomImage = function(images){
+    // creates a random number for the index in the array that is multiplied by the number of images then rounds down
+    const randomIndex = Math.floor(Math.random() * images.length);
+    //console.log(randomIndex);
+
+    const randomImage = images[randomIndex];
+    //console.log(randomImage);
+}
